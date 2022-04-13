79d3001caa532cb939f66194f144aea40e0da39a
Young Yang <jyang@stust.edu.tw>
2022-04-14 00:35:56 +0800

    jyang

---
+++ Makefile
@@ -0,0 +1,13 @@
+all: main
+
+main:
+	g++ -std=c++11 main.cpp -o main
+	chmod +x ./check.py
+
+test1: main
+	./main | ./ta/check.py
+	
+test: test1
+
+clean:
+	rm main
---
+++ ta/check.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python
+import sys
+
+
+def main():
+    expected = "Hello Github Classroom"
+    result = ""
+    for line in sys.stdin:
+        result += line
+        sys.stdout.write(line)
+    result = result.strip()
+    if result == expected:
+        print("Pass")
+        exit(0)
+    print(f"Your Output : {result}")
+    print(f"Expected    : {expected}")
+    exit(1)
+
+
+if __name__ == "__main__":
+    main()
